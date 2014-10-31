6f683839100fa239b3c5dbb13b99f643193fbcf4
ml242 <mattlucas@gmail.com>
2014-10-31 13:33:48 -0400

    right button is now working my pokemon filmstrip

--- js/main.js
+++ js/main.js
@@ -4,4 +4,21 @@
 $(function(){
 	// sanity check
 	console.log("hey, i'm all up in your app making behaviours happen on your computer");
+
+
+// "jQuery please bind an event to the object with a class of left-button and then do some stuff"
+$(".left-button").on("click", function(){
+	// stuff goes here
+});
+
+$(".right-button").on("click", function(){
+	// sanity check on the function
+	console.log('right button reporting form duty');
+	// select the filmstrip with jQuery and assign it to a a variable.
+	var filmstrip = $('.filmstrip');
+	// change the css on each click, negative ='s is going to subtract another 100px's each time
+	filmstrip.css({'margin-left': '-=15px'});
+});
+
+
 });
